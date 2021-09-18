d5c32513678a85bd08f7d23186a42c29bf4bcc4b
Shirley Liu <sl6895@nyu.edu>
2021-09-18 15:07:47 +0800

    changed anchor link transition

---
+++ labs/lab-03/animation1/script.js
@@ -0,0 +1,11 @@
+let button = document.getElementById("button");
+let box = document.getElementById("box");
+
+
+let boxAngle = 0;
+
+
+button.addEventListener("click", ()=>{
+    boxAngle = boxAngle + 360;
+    box.style.transform = "rotate(" + boxAngle +"deg)";
+})
